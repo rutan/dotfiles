ec251b164db870ce17f9c0b5041d67604c02d34f
rutan <ru_shalm@hazimu.com>
2025-08-15 21:31:26 +0900

    remove setup command

--- setup.sh
+++ setup.sh
@@ -2,14 +2,6 @@
 
 dotfiles_path=`cd $(dirname $0) && pwd`
 
-# neobundle
-if test -e ~/.vim/bundle/neobundle.vim; then
-    echo 'skip neobundle'
-else
-    mkdir -p ~/.vim/bundle
-    git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
-fi
-
 # link
 ln -s ${dotfiles_path}/.gitignore ~/.gitignore
 ln -s ${dotfiles_path}/.gitconfig ~/.gitconfig
